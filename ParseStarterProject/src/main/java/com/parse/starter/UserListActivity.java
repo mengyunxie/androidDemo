78f0a1deb6a72bca9c81a44089622479d1e2a88e
Miya <mengyunxie@Miyas-MacBook-Air.local>
2022-10-10 19:12:13 -0700

    Showing the user List.

--- ParseStarterProject/src/main/java/com/parse/starter/UserListActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/UserListActivity.java
@@ -5,7 +5,13 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class UserListActivity extends AppCompatActivity {
 
@@ -14,10 +20,30 @@ public class UserListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_list);
 
-        ListView userListView = (ListView) findViewById(R.id.userListView);
-        ArrayList<String> usernames = new ArrayList<>();
-        usernames.add("test");
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, usernames);
-        userListView.setAdapter(arrayAdapter);
+        final ListView userListView = (ListView) findViewById(R.id.userListView);
+        final ArrayList<String> usernames = new ArrayList<>();
+        final ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, usernames);
+
+        ParseQuery<ParseUser> query = ParseUser.getQuery();
+        query.whereNotEqualTo("username", ParseUser.getCurrentUser().getUsername());
+        query.addAscendingOrder("username");
+        query.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> objects, ParseException e) {
+                if(e == null) {
+                    if(objects.size() > 0) {
+                        for(ParseUser user : objects) {
+                            usernames.add(user.getUsername());
+                        }
+                        userListView.setAdapter(arrayAdapter);
+                    }
+                } else {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+
     }
 }
